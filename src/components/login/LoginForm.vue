5f74b4e8213db5c52c3784e8157056e936f2713f
Gabriel Umbelino <gabriel.umbelino@hitechnologies.com.br>
2019-02-05 08:22:09 -0200

    add login form component

---
+++ src/components/login/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-card title="Card Title" tag="article" style="max-width: 20rem;" class="mb-2">
+    <p
+      class="card-text"
+    >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "LoginForm"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
