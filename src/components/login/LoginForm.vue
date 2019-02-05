e39dfd9ff1cfe18fd2e2533f23a43450d0dd080b
Gabriel Umbelino <gabriel.umbelino@hitechnologies.com.br>
2019-02-05 08:27:48 -0200

    add header to login form component

--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -1,8 +1,10 @@
 <template>
   <b-card title="Card Title" tag="article" style="max-width: 20rem;" class="mb-2">
-    <p
-      class="card-text"
-    >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div slot="header">
+      <h1>Log in to our site</h1>
+      <p>Enter username or password to log on</p>
+    </div>
+
     <b-button href="#" variant="primary">Go somewhere</b-button>
   </b-card>
 </template>
