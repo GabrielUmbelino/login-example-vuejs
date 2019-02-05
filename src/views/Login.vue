0535b8c6e618c045b107beb30792e0e6f3583560
Gabriel Umbelino <gabriel.umbelino@hitechnologies.com.br>
2019-02-05 08:21:54 -0200

    create login page

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col xs="12" sm="10" offset-sm="1" lg="8" offset-lg="2" xl="4" offset-xl="4">
+        <login-form @loginEvent="login"></login-form>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import LoginForm from "@/components/login/LoginForm";
+export default {
+  components: {
+    LoginForm
+  },
+  methods: {
+    login(user) {
+      console.log(user);
+    }
+  }
+};
+</script>
