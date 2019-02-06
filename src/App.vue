a0d81f34c3264ab99d13a5c97d7213aaf0da12b7
Gabriel Umbelino <gabriel.umbelino@hitechnologies.com.br>
2019-02-05 23:38:11 -0200

    add fonts and overlay style

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,19 @@
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .overlay {
+    background-color: #757ec9;
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
   }
 }
 </style>
