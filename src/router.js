46c0f545b7b92a875bae42b1fd57b7d6a0eaeada
Gabriel Umbelino <gabriel.umbelino@hitechnologies.com.br>
2019-02-05 23:39:39 -0200

    add navigation guard for home router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import store from "./store"
 
 Vue.use(Router)
 
@@ -10,15 +11,19 @@ export default new Router({
   routes: [{
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      beforeEnter: (to, from, next) => {
+        if (store.getters.loggedIn) {
+          next()
+        } else {
+          next("/login")
+        }
+      }
     },
     {
       path: '/login',
       name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import( /* webpackChunkName: "about" */ './views/Login.vue')
+      component: () => import('./views/Login.vue')
     }
   ]
 })
