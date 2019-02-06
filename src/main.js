c721fd7ac1b71a652c6ae6664ab3eda20a89e84b
Gabriel Umbelino <gabriel.umbelino@hitechnologies.com.br>
2019-02-05 23:38:45 -0200

    axios configuration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import App from './App.vue'
+import axios from "axios";
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -6,6 +7,7 @@ import router from './router'
 import store from './store'
 import Vue from 'vue'
 
+axios.defaults.baseURL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty'
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
