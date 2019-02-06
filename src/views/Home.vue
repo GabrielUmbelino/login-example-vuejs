c1f95df2f823ca600377e2cbc2b23aca4a3647e7
Gabriel Umbelino <gabriel.umbelino@hitechnologies.com.br>
2019-02-05 23:33:24 -0200

    Home page updates
    add welcome message after login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="home">hi you were succesfully logedin.</div>
+  <b-container class="home">
+    <b-row class="p-3">
+      <b-col xs="12" sm="10" offset-sm="1" lg="8" offset-lg="2" xl="4" offset-xl="4">
+        <b-alert show variant="success">Hi you were successfully logged in.</b-alert>
+      </b-col>
+    </b-row>
+    <div class="overlay"></div>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
   name: "home"
 };
