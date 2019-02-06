6e6e02a6dcb4a01b15aa158ef44fc9046b0cdb15
Gabriel Umbelino <gabriel.umbelino@hitechnologies.com.br>
2019-02-05 23:40:10 -0200

    add session state manangement

--- src/store.js
+++ src/store.js
@@ -5,12 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    userId: null,
+    token: null
   },
   mutations: {
-
+    "LOGIN"(state, userData) {
+      state.userId = userData.userId;
+      state.token = userData.token;
+    }
+  },
+  getters: {
+    loggedIn(state) {
+      return Boolean(state.token)
+    }
   },
   actions: {
-
+    login({
+      commit
+    }, userData) {
+      commit("LOGIN", userData)
+    }
   }
 })
